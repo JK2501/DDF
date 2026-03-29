34172e2d820463bd5e928a2305086d90519282c1
Jannes Kroell <Jannes.Kroell@web.de>
2026-03-29 20:18:58 +0200

    Improved sprite loc function>

--- src/main/java/guess_ddf/web/sprite/SpriteUtil.java
+++ src/main/java/guess_ddf/web/sprite/SpriteUtil.java
@@ -5,40 +5,34 @@ import org.springframework.stereotype.Component;
 @Component
 public class SpriteUtil {
 
-    private final int X = 68;
-    private final int Y = 68;
+    private static final int TILE_SIZE = 68;
 
-    private final String SPRITE_0 = "/cover/tile_00.webp";
-    private final String SPRITE_1 = "/cover/tile_01.webp";
-    private final String SPRITE_2 = "/cover/tile_02.webp";
+    private static final String[] SPRITES = {
+            "/cover/tile_00.webp",
+            "/cover/tile_01.webp",
+            "/cover/tile_02.webp"
+    };
 
     public String getSpriteURL(int number) {
-        String numberAsString = String.format("%03d", number);
-        int d3 = numberAsString.charAt(0) - '0';
-        if(d3 == 0){ return SPRITE_0; }
-        if(d3 == 1){ return SPRITE_1; }
-        if(d3 == 2){ return SPRITE_2; }
-        return SPRITE_0;
+        int d3 = number / 100; // hundreds digit
+        return d3 < SPRITES.length ? SPRITES[d3] : SPRITES[0];
     }
 
     public int getXLocOnSprite(int number) {
-        String numberAsString = String.format("%03d", number);
-        int d1 = numberAsString.charAt(2) - '0';
-        return (-1) * (Y) * (d1);
+        int d1 = number % 10; // ones digit
+        return -TILE_SIZE * d1;
     }
 
     public int getYLocOnSprite(int number) {
-        String numberAsString = String.format("%03d", number);
-        int d2 = numberAsString.charAt(1) - '0';
-        return (-1) * (Y) * (d2);
+        int d2 = (number / 10) % 10; // tens digit
+        return -TILE_SIZE * d2;
     }
 
     public String getCombinedStyling(int number) {
-        return String.format("background: url(%s); background-position: %dpx %dpx",
-                getSpriteURL(number),
-                getXLocOnSprite(number),
-                getYLocOnSprite(number)
-        );
-    }
+        int x = getXLocOnSprite(number);
+        int y = getYLocOnSprite(number);
 
+        return "background: url(" + getSpriteURL(number) + "); " +
+                "background-position: " + x + "px " + y + "px";
+    }
 }
